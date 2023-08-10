6ee4b293e625841d3faa873c2aedf27e4eb1bf0c
Chema Muñoz <chemunoz78@gmail.com>
2023-08-10 08:54:59 +0200

    chore: minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner:
+          false, // Quita la etiqueta "debug" del simulador
       home: Scaffold(body: Center(child: Text('Hola mundo'))),
     );
   }
