546fae8b4fc559f86567f00f449b7a033908418f
Chema Muñoz <chemunoz78@gmail.com>
2023-09-20 16:16:53 +0200

    feat: add "plus one" button functionality

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +19,13 @@ class CounterScreen extends StatelessWidget {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
+            children: [
               Text(
-                '10',
-                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+                '$clickCounter',
+                style:
+                    const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
               ),
-              Text(
+              const Text(
                 'Clicks',
                 style: TextStyle(fontSize: 25),
               )
@@ -25,7 +33,14 @@ class CounterScreen extends StatelessWidget {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            // Para que la información se renderice en la vista
+            // tenemos que indidar que recargue el Widget
+            // así que metemos el incremento en el método de recarga
+            setState(() {
+              clickCounter++;
+            });
+          },
           child: const Icon(Icons.plus_one),
         ));
   }
